4708c74a57f726a2f7e19b1e676196da0d660b14
Kotaro <sekikotaro@keio.jp>
2026-04-09 21:30:58 +0200

    Support Vercel KV provisioning

--- src/app/api/visitor/route.ts
+++ src/app/api/visitor/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server';
 import { Redis } from '@upstash/redis';
 
-const redis = (process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN)
+const redisUrl = process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL;
+const redisToken = process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN;
+
+const redis = (redisUrl && redisToken)
   ? new Redis({
-      url: process.env.UPSTASH_REDIS_REST_URL,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN,
+      url: redisUrl,
+      token: redisToken,
     })
   : null;
 
